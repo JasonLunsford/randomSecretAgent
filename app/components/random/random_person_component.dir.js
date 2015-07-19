47670894da4b9b1c2d3fc55bc7b4750aa417f0f9
Jason Lunsford <miyamota@yahoo.com>
2015-07-19 09:15:49 -0400

    first round UI pass, all data in place

--- app/components/random/random_person_component.dir.js
+++ app/components/random/random_person_component.dir.js
@@ -6,18 +6,27 @@ angular.module('randomPersonChallengeApp.random_person_component', [])
             templateUrl : 'components/random/random_person_component.tpl.html',
             controller  : function($scope) {
             	RandomPersonService.getRandomPerson().then(function(data) {
-            		$scope.ourSecretAgent = data.results[0].user;
-            		$scope.dateOfBirth = moment.unix(data.results[0].user.dob).format("MMMM Do YYYY");
+            		var agentData = data.results[0].user;
+
+            		$scope.ourSecretAgent = agentData;
+            		$scope.presumedName = agentData.name.title+" "+agentData.name.first+" "+agentData.name.last;
+            		$scope.dateOfBirth = moment.unix(agentData.dob).format("MM-DD-YYYY");
+
             		BaconIpsumService.getRandomCopy().then(function(copyData) {
             			$scope.randomDossier = copyData[0];
+            		}).catch(function(err) {
+            			$scope.randomDossier = "Classified!";
             		});
             	});
             },
             link		: function(scope, element, attribs) {
             	scope.fetchAgent = function() {
 	            	RandomPersonService.getRandomPerson().then(function(data) {
-	            		scope.ourSecretAgent = data.results[0].user;
-	            		scope.dateOfBirth = moment.unix(data.results[0].user.dob).format("MMMM Do YYYY");
+	            		var agentData = data.results[0].user;
+
+	            		scope.ourSecretAgent = agentData;
+	            		scope.presumedName = agentData.name.title+" "+agentData.name.first+" "+agentData.name.last;
+	            		scope.dateOfBirth = moment.unix(agentData.dob).format("MM-DD-YYYY");
 	            		BaconIpsumService.getRandomCopy().then(function(copyData) {
             				scope.randomDossier = copyData[0];
             			});
