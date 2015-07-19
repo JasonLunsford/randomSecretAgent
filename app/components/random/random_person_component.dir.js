cc73dc9686a8b47c157f3802fa54f379c29a90fd
Jason Lunsford <miyamota@yahoo.com>
2015-07-19 16:40:26 -0400

    solid version

--- app/components/random/random_person_component.dir.js
+++ app/components/random/random_person_component.dir.js
@@ -1,6 +1,7 @@
 angular.module('randomPersonChallengeApp.random_person_component', [])
 
-.directive('randomPersonComponent', ['RandomPersonService', 'BaconIpsumService', function(RandomPersonService, BaconIpsumService) {
+.directive('randomPersonComponent', ['RandomPersonService', 'BaconIpsumService',
+	function(RandomPersonService, BaconIpsumService) {
         return {
             restrict    : 'EA',
             scope		: {
@@ -12,7 +13,7 @@ angular.module('randomPersonChallengeApp.random_person_component', [])
             		var agentData = data.results[0].user;
 
             		$scope.ourSecretAgent = agentData;
-            		$scope.presumedName = agentData.name.title+" "+agentData.name.first+" "+agentData.name.last;
+            		$scope.presumedName = '"'+agentData.name.title+" "+agentData.name.first+" "+agentData.name.last+'"';
             		$scope.dateOfBirth = moment.unix(agentData.dob).format("MM-DD-YYYY");
 
             		BaconIpsumService.getRandomCopy().then(function(copyData) {
