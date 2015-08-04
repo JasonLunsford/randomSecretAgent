9dfa7a1a8f25a785e094866c0ec4e285fed06e6d
Jason Lunsford <miyamota@yahoo.com>
2015-08-04 11:55:34 -0400

    removed outdated factories

--- app/components/services/baconIpsumApiFactory.js
+++
@@ -1,8 +0,0 @@
-angular.module('randomPersonChallengeApp.services');
-/*	.factory("BaconIpsumApiFactory", ["Restangular",
-		function(restangular) {
-
-		return restangular.withConfig(function(RestangularConfigurer) {
-		    RestangularConfigurer.setBaseUrl("https://baconipsum.com");
-		});
-}]);*/
--- app/components/services/randomPersonApiFactory.js
+++
@@ -1,8 +0,0 @@
-angular.module('randomPersonChallengeApp.services');
-/*	.factory("RandomPersonApiFactory", ["Restangular",
-		function(restangular) {
-
-		return restangular.withConfig(function(RestangularConfigurer) {
-		    RestangularConfigurer.setBaseUrl("https://randomuser.me");
-		});
-}]);*/
